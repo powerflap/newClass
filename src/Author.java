68a8aafe021ce0906e209e1aedf3ff1b9be878e4
Женёк <muratov_yevgeniy@vk.com>
2024-12-26 20:14:02 +0300

    Final commit

--- src/Author.java
+++ src/Author.java
@@ -1,16 +1,40 @@
-public class Author {
-   private String name;
-   private String surname;
+import java.util.Objects;
 
-public Author(String name,String surname){
-    this.name=name;
-    this.surname=surname;
-}
+public class Author {
+    private String name;
+    private String surname;
+
+    public Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
     public String getName() {
         return this.name;
     }
+
     public String getSurname() {
         return this.surname;
     }
 
+    @Override
+    public String toString() {
+        return name + " " + surname;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+
+        Author other = (Author) obj;
+        return Objects.equals(name, other.name) && Objects.equals(surname, other.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
 }
